28cd6bdbaedfd968a69da86de8d582f30d5fd7db
AlirezaKeshavarz100 <79831060+AlirezaKeshavarz100@users.noreply.github.com>
2026-06-29 11:44:57 +0330

    Update fetch_prices.py

--- fetch_prices.py
+++ fetch_prices.py
@@ -1,1 +1,11 @@
-print("NEW EXECUTION WORKING")
+import yfinance as yf
+
+print("STARTING DATA FETCH...")
+
+ticker = yf.Ticker("ZC=F")
+
+data = ticker.history(period="1d")
+
+print(data)
+
+print("DONE")
