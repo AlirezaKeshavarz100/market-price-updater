4d05828f2ba789854adc81d1d2203f4f0a2e30d4
AlirezaKeshavarz100 <79831060+AlirezaKeshavarz100@users.noreply.github.com>
2026-06-29 11:19:41 +0330

    Update fetch_prices.py

--- fetch_prices.py
+++ fetch_prices.py
@@ -1,7 +1,11 @@
 import yfinance as yf
 
-ticker = yf.Ticker("ZC=F")
-
 print("Getting price...")
 
-print(ticker.fast_info)
+ticker = yf.Ticker("ZC=F")
+
+data = ticker.history(period="1d")
+
+print(data)
+
+print("Done")
