77f5e41b532d88b1d24f06b67e2d6d508688ff0b
AlirezaKeshavarz100 <79831060+AlirezaKeshavarz100@users.noreply.github.com>
2026-06-29 11:27:26 +0330

    Update fetch_prices.py

--- fetch_prices.py
+++ fetch_prices.py
@@ -1,11 +1,1 @@
-import yfinance as yf
-
-print("Getting price...")
-
-ticker = yf.Ticker("ZC=F")
-
-data = ticker.history(period="1d")
-
-print(data)
-
-print("Done")
+print("VERSION 2 - REAL EXECUTION TEST")
